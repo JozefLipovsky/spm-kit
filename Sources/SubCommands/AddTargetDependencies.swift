65f79a16ed8925e58c34d5cd53c4789bb088a7d2
Jozef Lipovsky <j.lipovsky@me.com>
2026-05-26 19:48:11 -0700

    Add AddTargetDependencies command scaffolding

---
+++ Sources/SubCommands/AddTargetDependencies.swift
@@ -0,0 +1,14 @@
+//
+//  AddTargetDependencies.swift
+//  SPMKit
+//
+//  Created by Jozef Lipovsky on 2026-05-26.
+//
+
+import ArgumentParser
+import Core
+import Foundation
+
+package struct AddTargetDependencies: AsyncParsableCommand {
+    package init() {}
+}
