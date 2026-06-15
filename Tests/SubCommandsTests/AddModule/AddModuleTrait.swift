84e424dc86e0a80b38a30430ca34e645e5ca5db5
Jozef Lipovsky <j.lipovsky@me.com>
2026-06-14 20:28:01 -0700

    AddModuleTrait cleanup

--- Tests/SubCommandsTests/AddModule/AddModuleTrait.swift
+++ Tests/SubCommandsTests/AddModule/AddModuleTrait.swift
@@ -36,7 +36,6 @@ struct AddModuleTrait: TestTrait, TestScoping {
         self.clientErrorStub = clientErrorStub
     }
 
-    // swiftlint:disable function_body_length
     func provideScope(
         for test: Test,
         testCase: Test.Case?,
@@ -54,74 +53,38 @@ struct AddModuleTrait: TestTrait, TestScoping {
         try configClientStubs.generateConfig(at: pathStub.currentPath)
 
         try await withDependencies {
-            $0.pathClient.current = {
-                currentPathStub.path
-            }
-            $0.nooraClient.textInput = { configuration, argument in
-                await executionContext.nooraClientSpy.recordTextInput(configuration: configuration, argument: argument)
-                return nooraClientStubs.moduleName
-            }
-            $0.nooraClient.productTypeSelection = { configuration, argument in
-                await executionContext.nooraClientSpy.recordProductTypeSelection(
-                    configuration: configuration,
-                    productType: argument
-                )
-                return nooraClientStubs.productType
-            }
-            $0.nooraClient.testingLibrarySelection = { configuration, argument in
-                await executionContext.nooraClientSpy.recordTestingLibrarySelection(
-                    configuration: configuration,
-                    testingLibrary: argument
-                )
-                return nooraClientStubs.testingLibrary
-            }
-            $0.nooraClient.yesOrNoConfirmation = { configuration, shouldSkip in
-                await executionContext.nooraClientSpy.recordYesOrNoConfirmation(
-                    configuration: configuration,
-                    shouldSkip: shouldSkip
-                )
-                return nooraClientStubs.selectDependencies
-            }
-            $0.nooraClient.dependenciesSelection = { configuration, options in
-                await executionContext.nooraClientSpy.recordDependenciesSelection(
-                    configuration: configuration,
-                    options: options
-                )
-                return nooraClientStubs.dependencies
-            }
-            $0.nooraClient.progress = { message, successMessage, errorMessage, operation in
-                await executionContext.nooraClientSpy.recordOperationProgress(
-                    message: message,
-                    successMessage: successMessage,
-                    errorMessage: errorMessage
-                )
-                return try await operation { _ in }
-            }
-            $0.subprocessClient.run = { command, workingDirectory in
-                if let clientErrorStub, case .subprocessClient = clientErrorStub { throw clientErrorStub }
-                await executionContext.subprocessClientSpy.recordRun(
-                    command: command,
-                    workingDirectory: workingDirectory
-                )
-            }
-            $0.subprocessClient.runAndCapture = { command, workingDirectory in
-                if let clientErrorStub, case .subprocessClient = clientErrorStub { throw clientErrorStub }
-                await executionContext.subprocessClientSpy.recordRunAndCapture(
-                    command: command,
-                    workingDirectory: workingDirectory
-                )
-                return subprocessClientStubs.result(for: command)
-            }
-            $0.configClient.modulesPath = { configPath in
-                if let clientErrorStub, case .configClient = clientErrorStub { throw clientErrorStub }
-                await executionContext.configClientSpy.recordModulesPath(atConfigPath: configPath.string)
-                return configClientStubs.modulesPath.path
-            }
-            $0.configClient.swiftFormatConfigPath = { configPath in
-                if let clientErrorStub, case .configClient = clientErrorStub { throw clientErrorStub }
-                await executionContext.configClientSpy.recordSwiftFormatConfigPath(atConfigPath: configPath.string)
-                return configClientStubs.swiftFormatConfigPath.path
-            }
+            $0.currentPath(currentPathStub)
+            $0.textInput(executionContext: executionContext, moduleNameStub: nooraClientStubs.moduleName)
+            $0.productTypeSelection(executionContext: executionContext, productTypeStub: nooraClientStubs.productType)
+            $0.testingLibrarySelection(
+                executionContext: executionContext,
+                testingLibraryStub: nooraClientStubs.testingLibrary
+            )
+            $0.yesOrNoConfirmation(
+                executionContext: executionContext,
+                selectDependenciesStub: nooraClientStubs.selectDependencies
+            )
+            $0.dependenciesSelection(
+                executionContext: executionContext,
+                dependenciesStub: nooraClientStubs.dependencies
+            )
+            $0.progress(executionContext: executionContext)
+            $0.runCommand(executionContext: executionContext, clientErrorStub: clientErrorStub)
+            $0.runAndCaptureCommand(
+                executionContext: executionContext,
+                subprocessClientStubs: subprocessClientStubs,
+                clientErrorStub: clientErrorStub
+            )
+            $0.modulesPath(
+                executionContext: executionContext,
+                configClientStubs: configClientStubs,
+                clientErrorStub: clientErrorStub
+            )
+            $0.swiftFormatConfigPath(
+                executionContext: executionContext,
+                configClientStubs: configClientStubs,
+                clientErrorStub: clientErrorStub
+            )
         } operation: { [executionContext, pathStub] in
             try await AddModuleExecutionContext.$current.withValue(executionContext) {
                 try await function()
@@ -129,7 +92,6 @@ struct AddModuleTrait: TestTrait, TestScoping {
             }
         }
     }
-    // swiftlint:enable function_body_length
 }
 
 extension Trait where Self == AddModuleTrait {
@@ -175,6 +137,134 @@ extension AddModuleTrait {
             self.testingLibrary = testingLibrary
             self.selectDependencies = selectDependencies
             self.dependencies = dependencies
+        }
+    }
+}
+
+private extension DependencyValues {
+    mutating func currentPath(_ currentPath: String) {
+        pathClient.current = { currentPath.path }
+    }
+
+    mutating func textInput(executionContext: AddModuleExecutionContext, moduleNameStub: String) {
+        nooraClient.textInput = { configuration, argument in
+            await executionContext.nooraClientSpy.recordTextInput(configuration: configuration, argument: argument)
+            return moduleNameStub
+        }
+    }
+
+    mutating func productTypeSelection(executionContext: AddModuleExecutionContext, productTypeStub: ProductType) {
+        nooraClient.productTypeSelection = { configuration, argument in
+            await executionContext.nooraClientSpy.recordProductTypeSelection(
+                configuration: configuration,
+                productType: argument
+            )
+
+            return productTypeStub
+        }
+    }
+
+    mutating func testingLibrarySelection(
+        executionContext: AddModuleExecutionContext,
+        testingLibraryStub: TestingLibrary
+    ) {
+        nooraClient.testingLibrarySelection = { configuration, argument in
+            await executionContext.nooraClientSpy.recordTestingLibrarySelection(
+                configuration: configuration,
+                testingLibrary: argument
+            )
+            
+            return testingLibraryStub
+        }
+    }
+
+    mutating func yesOrNoConfirmation(executionContext: AddModuleExecutionContext, selectDependenciesStub: Bool) {
+        nooraClient.yesOrNoConfirmation = { configuration, argument in
+            await executionContext.nooraClientSpy.recordYesOrNoConfirmation(
+                configuration: configuration,
+                shouldSkip: argument
+            )
+
+            return selectDependenciesStub
+        }
+    }
+
+    mutating func dependenciesSelection(
+        executionContext: AddModuleExecutionContext,
+        dependenciesStub: [PackageDependency]
+    ) {
+        nooraClient.dependenciesSelection = { configuration, options in
+            await executionContext.nooraClientSpy.recordDependenciesSelection(
+                configuration: configuration,
+                options: options
+            )
+
+            return dependenciesStub
+        }
+    }
+
+    mutating func progress(executionContext: AddModuleExecutionContext) {
+        nooraClient.progress = { message, successMessage, errorMessage, operation in
+            await executionContext.nooraClientSpy.recordOperationProgress(
+                message: message,
+                successMessage: successMessage,
+                errorMessage: errorMessage
+            )
+
+            return try await operation { _ in }
+        }
+    }
+
+    mutating func runCommand(
+        executionContext: AddModuleExecutionContext,
+        clientErrorStub: AddModuleTrait.ClientErrorStub?
+    ) {
+        subprocessClient.run = { command, workingDirectory in
+            if let clientErrorStub, case .subprocessClient = clientErrorStub { throw clientErrorStub }
+            await executionContext.subprocessClientSpy.recordRun(
+                command: command,
+                workingDirectory: workingDirectory
+            )
+        }
+    }
+
+    mutating func runAndCaptureCommand(
+        executionContext: AddModuleExecutionContext,
+        subprocessClientStubs: SubprocessClientStubs,
+        clientErrorStub: AddModuleTrait.ClientErrorStub?
+    ) {
+        subprocessClient.runAndCapture = { command, workingDirectory in
+            if let clientErrorStub, case .subprocessClient = clientErrorStub { throw clientErrorStub }
+            await executionContext.subprocessClientSpy.recordRunAndCapture(
+                command: command,
+                workingDirectory: workingDirectory
+            )
+
+            return subprocessClientStubs.result(for: command)
+        }
+    }
+
+    mutating func modulesPath(
+        executionContext: AddModuleExecutionContext,
+        configClientStubs: ConfigFileStub,
+        clientErrorStub: AddModuleTrait.ClientErrorStub?
+    ) {
+        configClient.modulesPath = { configPath in
+            if let clientErrorStub, case .configClient = clientErrorStub { throw clientErrorStub }
+            await executionContext.configClientSpy.recordModulesPath(atConfigPath: configPath.string)
+            return configClientStubs.modulesPath.path
+        }
+    }
+
+    mutating func swiftFormatConfigPath(
+        executionContext: AddModuleExecutionContext,
+        configClientStubs: ConfigFileStub,
+        clientErrorStub: AddModuleTrait.ClientErrorStub?
+    ) {
+        configClient.swiftFormatConfigPath = { configPath in
+            if let clientErrorStub, case .configClient = clientErrorStub { throw clientErrorStub }
+            await executionContext.configClientSpy.recordSwiftFormatConfigPath(atConfigPath: configPath.string)
+            return configClientStubs.swiftFormatConfigPath.path
         }
     }
 }
